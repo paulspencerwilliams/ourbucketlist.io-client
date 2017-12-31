1fdfd82b67063c4fb7d996d73a6af47233bd13ab
Paul Williams <paul@paulswilliams.me.uk>
2017-12-31 16:01:58 +0000

    ignore linting issues within registerServiceWorker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -13,7 +13,7 @@ const isLocalhost = Boolean(window.location.hostname === 'localhost' ||
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
-
+/* eslint-disable no-console, no-param-reassign, no-use-before-define */
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
